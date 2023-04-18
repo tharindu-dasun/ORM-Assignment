c5794951b82cc544ba775b5735cc0cb4a1220ad5
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-04-18 17:10:38 +0530

    update 0.7

--- src/lk/ijse/gdse/controller/ReservationFormController.java
+++ src/lk/ijse/gdse/controller/ReservationFormController.java
@@ -1,4 +1,39 @@
 package lk.ijse.gdse.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ReservationFormController {
+    public JFXTextField txtReservationId;
+    public JFXTextField txtStudentId;
+    public JFXButton btnSearchReservation;
+    public JFXTextField txtQty;
+    public JFXButton btnSaveReservation;
+    public JFXButton btnUpdateReservation;
+    public JFXButton btnClearReservation;
+    public JFXButton btnDeleteReservation;
+    public TableView tblReservation;
+    public TableColumn colReservationId;
+    public TableColumn colStudentId;
+    public TableColumn colRoomTypeIdRes;
+    public TableColumn colDate;
+    public TableColumn colQty;
+
+    public void SearchReservationOnAction(ActionEvent event) {
+    }
+
+    public void SaveReservationOnAction(ActionEvent event) {
+    }
+
+    public void UpdateReservationOnAction(ActionEvent event) {
+    }
+
+    public void ClearReservationOnAction(ActionEvent event) {
+    }
+
+    public void DeleteReservationOnAction(ActionEvent event) {
+    }
 }
